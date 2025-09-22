490a0e16dac3d4c465e524b06bd7e797591f4cf2
aryan kumar shukla <ara12shukla@gmail.com>
2025-09-22 23:07:24 +0530

    task title

---
+++ script.js
@@ -0,0 +1,22 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    let timerInterval;
+    let totalSeconds = 0;
+    let remainingSeconds = 0;
+
+    function saveTasks() {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      renderTasks();
+    }
+
+    function addTask() {
+      let title = prompt("Enter task title:");
+      if (!title) return;
+      tasks.push({
+        title: title,
+        status: "Not Started",
+        priority: "Medium",
+        dueDate: "",
+        doNow: false
+      });
+      saveTasks();
+    }
